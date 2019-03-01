f24a7dd2cc2b7267e9ca400d16f793bb0822bc40
Sachin Mahajan <sachinm78@gmail.com>
2019-03-01 21:41:32 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,5 @@
 def my_select(array)
-  i = 0
+  i = 0 
   select = []
-  while i < array.length
-    if yield(array[i])
-      select << array[i]
-    end
-    i+=1
-  end
-  select
+  if select 
 end
